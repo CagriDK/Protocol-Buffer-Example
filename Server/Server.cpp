fddc6465048f6dd2a01e738632310e83b404109e
Çağrı Dükünlü <cagridknl@gmail.com>
2023-09-14 09:53:26 +0300

    Echo-Server tcp structure builded Read the buffer and return to the client right away.

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,5 +1,16 @@
 #include "Server.h"
+#include <thread>
 
 int main(int, char**){
-    std::cout << "Hello, from ServerProtocolBufferP!\n";
+    try{
+        cout<<"Server is starting...\n";
+        boost::asio::io_context io_context;
+        Server server(io_context,8080);
+        std::thread serverThread([&io_context](){io_context.run();});
+        serverThread.join();
+    }
+    catch(std::exception& all)
+    {
+        cout<<"The Catch = "<<all.what()<<"\n";
+    }
 }
--- Server/Server.h
+++ Server/Server.h
@@ -4,12 +4,61 @@
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
+using boost::asio::ip::tcp;
+using std::string;
+using std::cout;
+using std::endl;
 
-class Server{
-public:
-    Server(int port,std::string IpAddr);
+class Session : public std::enable_shared_from_this<Session> {
+    public:
+        Session(tcp::socket socket) : socket_(std::move(socket)) {}
+
+        void start() {
+            doRead();
+        }
 
 private:
-    int m_port;
-    std::string IpAddr;
+    void doRead() {
+        auto self(shared_from_this());
+        boost::asio::async_read_until(socket_, buf_, "\n", [this, self](boost::system::error_code ec, std::size_t length) {
+            if (!ec) {
+                doWrite(length);
+                doRead();
+            }
+        });
+    }
+
+    void doWrite(std::size_t length) {
+        auto self(shared_from_this());
+        // Write the exact message received back to the client
+        boost::asio::async_write(socket_, buf_.data(), 
+        [this,length, self](boost::system::error_code ec, std::size_t) {
+            if (!ec) {
+                buf_.consume(length); // Remove the written data
+            }
+        });
+    }
+
+    tcp::socket socket_;
+    boost::asio::streambuf buf_;
+    };
+class Server{
+public:
+    Server(boost::asio::io_context& io_context, unsigned short port)
+     :  acceptor_(io_context,tcp::endpoint(tcp::v4(),port)){
+        doAccept();
+     }
+private:
+    void doAccept() {
+            acceptor_.async_accept([this](boost::system::error_code ec, tcp::socket socket) {
+                if (!ec) {
+                    std::make_shared<Session>(std::move(socket))->start();
+                }
+                doAccept();
+            });
+        }
+
+    
+private:
+    tcp::acceptor acceptor_;
 };
