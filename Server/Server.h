469bcd8975178ac159398a3f3621b78713746472
Çağrı Dükünlü <cagridknl@gmail.com>
2023-09-19 10:47:54 +0300

    Server is handling proto message

--- Server/Server.h
+++ Server/Server.h
@@ -15,23 +15,65 @@ class Session : public std::enable_shared_from_this<Session> {
         Session(tcp::socket socket) : socket_(std::move(socket)) {}
 
         void start() {
-            doRead();
+            doReadHeader();
         }
 
 private:
-    void doRead() {
-        auto self(shared_from_this());
-        boost::asio::async_read(socket_, buf_, boost::asio::transfer_exactly(5), [this, self](boost::system::error_code ec, std::size_t length) {
+void doReadHeader() {
+    auto self(shared_from_this());
+    
+    buf_.prepare(sizeof(uint32_t));
+    
+    boost::asio::async_read(socket_, buf_, boost::asio::transfer_exactly(sizeof(uint32_t)), 
+        [this, self](boost::system::error_code ec, std::size_t length) {
             if (!ec) {
-                std::istream response_stream(&buf_);
-                std::string reply;
-                std::getline(response_stream, reply);
-                cout << "Readed from Client: " << reply << endl; 
-                buf_.consume(length);
-                doRead();
+                std::istream is(&buf_);
+                uint32_t messageSize;
+                is.read(reinterpret_cast<char*>(&messageSize), sizeof(uint32_t));
+                messageSize = ntohl(messageSize);
+                
+                buf_.consume(sizeof(uint32_t));  // Tüketiyoruz!
+                
+                doReadBody(messageSize);
+            } else {
+                std::cout << "Message error = " << ec.message() << std::endl;
             }
         });
-    }
+}
+
+void doReadBody(std::size_t messageSize) {
+    auto self(shared_from_this());
+    
+    buf_.prepare(messageSize);
+    
+    boost::asio::async_read(socket_, buf_, boost::asio::transfer_exactly(messageSize),
+        [this,messageSize, self](boost::system::error_code ec, std::size_t) {
+            if (!ec) {
+                std::istream is(&buf_);
+                std::vector<char> body(messageSize);
+                is.read(body.data(), messageSize);
+                
+                buf_.consume(messageSize);  // Tüketiyoruz!
+                
+                msg.ParseFromArray(body.data(), body.size());
+                std::cout<<"== Proto message datas ==\n";
+                std::cout << "id              = " << msg.id() << "\n";
+                std::cout << "name            = " << msg.name() << "\n";
+                std::cout << "account_balance = " << msg.account_balance() << "\n";
+                std::cout << "email           = " << msg.email() << "\n";
+                std::cout << std::endl;
+                doReadHeader();
+            } else {
+                std::cout << "Message error = " << ec.message() << std::endl;
+            }
+        });
+}
+
+
+
+
+
+
 
     // void doWrite(std::size_t length) {
     //     auto self(shared_from_this());
@@ -59,7 +101,7 @@ private:
             acceptor_.async_accept([this](boost::system::error_code ec, tcp::socket socket) {
                 if (!ec) {
                     std::make_shared<Session>(std::move(socket))->start();
-                    cout<<"Socket Connected";
+                    cout<<"Socket Connected \n";
                 }
                 doAccept();
             });
